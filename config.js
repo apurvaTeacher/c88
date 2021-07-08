c7b7f3ca2761ecc437471ded5a6d28ef49844e5b
apurvaTeacher <76532533+apurvaTeacher@users.noreply.github.com>
2021-07-08 15:24:35 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCxZXq6TrnomWZewiW1bOKKRFu3iEwJa_0",
+    authDomain: "stroyapp-ffa66.firebaseapp.com",
+    databaseURL: "https://stroyapp-ffa66-default-rtdb.firebaseio.com",
+    projectId: "stroyapp-ffa66",
+    storageBucket: "stroyapp-ffa66.appspot.com",
+    messagingSenderId: "420233311567",
+    appId: "1:420233311567:web:825ef00c593aff1133413a"
+  };
